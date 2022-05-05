2f20bd7b8f36f823f278a79359fa1001b3db9421
David Osuna <davidosuna@MacBook-Pro-de-David.local>
2022-05-05 15:29:18 +0200

    close navbar on click

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -7,31 +7,31 @@
     </div>
 
     <div class="tin-navbar__left">
-      <router-link class="tin-navbar__item is-logo" to="/">
+      <router-link class="tin-navbar__item is-logo" @click="opened = false" to="/">
         <img src="/tindefi-logo-full.svg" alt="Logo Tin Defi">
       </router-link>
     </div>
 
     <div class="tin-navbar__right">
-      <router-link class="tin-navbar__item" to="/services">{{t('navbar.services')}}</router-link>
+      <router-link class="tin-navbar__item" @click="opened = false" to="/services">{{t('navbar.services')}}</router-link>
 
       <MenuItemDropdown :title="t('navbar.resources')">
-        <div class="tin-navbar__dropdown_item">
+        <div class="tin-navbar__dropdown_item" @click="opened = false">
           <a :href="`/downloads/business-plan-1-${locale}.pdf`" target="_blank" class="tin-navbar__item"><TinIcon name="download" size="23px" /> {{t('pages.home.hero.buttons.business')}}</a>
         </div>
-        <div class="tin-navbar__dropdown_item">
+        <div class="tin-navbar__dropdown_item" @click="opened = false">
           <a :href="`/downloads/pitch-deck-1-${locale}.pdf`" target="_blank" class="tin-navbar__item"><TinIcon name="download" size="23px" /> {{t('pages.home.hero.buttons.pitch')}}</a>
         </div>
       </MenuItemDropdown>
 
-      <router-link class="tin-navbar__item" to="/about">{{t('navbar.about')}}</router-link>
-      <router-link class="tin-navbar__item" to="/faq">{{t('navbar.faq')}}</router-link>
+      <router-link class="tin-navbar__item" @click="opened = false" to="/about">{{t('navbar.about')}}</router-link>
+      <router-link class="tin-navbar__item" @click="opened = false" to="/faq">{{t('navbar.faq')}}</router-link>
 
       <template v-if="$route.name === 'ico'">
         <a v-if="walletStore.address" class="tin-navbar__item is-button tin-button wallet-connect-button is-connected" @click.prevent="walletStore.disconnect()">{{walletStore.shortAddress}}</a>
         <a v-else class="tin-navbar__item is-button tin-button wallet-connect-button" @click.prevent="walletStore.connect()">{{walletStore.loading ? t('forms.loading') : t('wallet.connect')}}</a>
       </template>
-      <router-link v-else class="tin-navbar__item is-button tin-button wallet-connect-button" to="/ico">{{t('navbar.join-ico')}}</router-link>
+      <router-link v-else class="tin-navbar__item is-button tin-button wallet-connect-button" @click="opened = false" to="/ico">{{t('navbar.join-ico')}}</router-link>
 
       <MenuItemDropdown position="right" simple class="lang-dropdown">
         <template #title>
